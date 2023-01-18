64352de646e24c92e43530107c1cb3405a63146d
nisha8c <nisha.chavan@appliedtechnology.se>
2023-01-18 10:08:50 +0100

    all routes and bootstrap conatiner set

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path='/' element={<h1>Hi</h1>}>Home</Route>
+        <Route path='/new' element={<h1>Hi</h1>}>New</Route>
+
+        <Route path='/:id' element={<h1>Hi</h1>}>
+          <Route index element={<h1>Show</h1>} />
+          <Route path='edit' element={<h1>Edit</h1>} />
+        </Route>
+
+        <Route path='*' element={<Navigate to='/'></Navigate>}>New</Route>
+      </Routes>
+    </Container>
+    
   );
 }
 
